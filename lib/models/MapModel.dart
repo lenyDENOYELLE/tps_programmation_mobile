8c4769735907d4978ed2c8152ba5b5a3381d3d36
lenyDENOYELLE <lenydenoyelle@gmail.com>
2025-02-10 17:14:42 +0100

    Ajout des méthodes init

--- lib/models/MapModel.dart
+++ lib/models/MapModel.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import './CaseModel.dart';
 class MapModel {
   int nbLine = 0;
@@ -11,5 +13,45 @@ class MapModel {
         List<CaseModel>.generate(nbCol, (int index) => CaseModel() , growable: false),
         growable: false
     );
+  }
+
+  void initBomb(){
+    int l;
+    int c;
+    int bombePlacees = 0;
+    while (bombePlacees < nbBomb){
+      l = Random().nextInt(nbLine);
+      c = Random().nextInt(nbCol);
+      if (!_cases[l][c].hasBomb){
+        _cases[l][c].hasBomb = true;
+        bombePlacees++;
+      }
+    }
+  }
+
+  CaseModel? tryGetCase(int l, int c){
+    if (c < 0 || c >= nbCol || l < 0 || l >= nbLine){
+      return null;
+    }
+    return _cases[l][c];
+  }
+
+  int computeNumber(int l, int c){
+    List<CaseModel?> voisins = [tryGetCase(l-1, c-1), tryGetCase(l-1, c), tryGetCase(l-1, c+1),
+                                tryGetCase(l, c-1), tryGetCase(l, c+1),
+                                tryGetCase(l+1, c-1), tryGetCase(l+1, c), tryGetCase(l+1, c+1)];
+    int compteur = 0;
+    for (CaseModel? casev in voisins){
+      if (casev != null){
+        if (casev.hasBomb){
+          compteur++;
+        }
+      }
+    }
+    return compteur;
+  }
+
+  void initNumbers(){
+
   }
 }
